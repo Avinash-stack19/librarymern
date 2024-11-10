4b96cd85e2c13f601461b912151e0f40a5f7c98f
Avinash-stack19 <ak2145363@gmail.com>
2024-11-10 18:54:22 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", {
+      const res = await axios.post("/api/auth/login", {
         username,
         password,
       });
